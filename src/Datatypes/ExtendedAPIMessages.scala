e3eafb1d18963ddcab1f1703b848bed9ae01f59a
David Gago <d.gago@campus.fct.unl.pt>
2017-05-20 17:11:09 +0100

    SearchEq + SearchNEq sem tolerancia a falhas e por testar

--- src/Datatypes/ExtendedAPIMessages.scala
+++ src/Datatypes/ExtendedAPIMessages.scala
@@ -6,3 +6,10 @@ import java.security.interfaces.RSAPublicKey;
 case class SumAll(nonce: Long, pos: Int, encrypted: Boolean, nsquare: BigInteger)
 case class SumMultAllResult(nonce: Long, res: BigInteger)
 case class MultAll(nonce: Long, pos: Int, encrypted: Boolean, key: RSAPublicKey)
+
+case class EntrySet(nonce: Long, set: List[Entry])
+case class SearchEq(nonce: Long, pos: Int, value: String, encrypted: Boolean, key: RSAPublicKey)
+case class SearchNEq(nonce: Long, pos: Int, value: String, encrypted: Boolean, key: RSAPublicKey)
+
+
+
